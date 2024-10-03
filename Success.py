a299daeaa7e4cb3f5eee466fae0cf707721f5de4
chanadana-2004 <22hr1a3213@mtieat.org>
2024-10-03 13:45:04 +0530

    Update Success.py

--- Success.py
+++ Success.py
@@ -14,3 +14,8 @@ p=input("Banerjee's Question:\n")
 r=input("Dhoni's Reply:\n")
 print("For Banerjee's Question \"{}\" Dhoni's Confident reply was \"{}\"".format(p,r))
 
+
+program:
+p = input("Banerjee's Question:\n")
+r = input("Dhoni's Reply:\n")
+print("For Banerjee's Question \"{}\" Dhoni's Confident reply was \"{}\"".format(p, r))
